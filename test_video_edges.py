dbccab2d6aef38f4d35e4fba92c7a4d07438e289
dixirion <khripunov.roman@gmail.com>
2017-07-11 18:41:50 +0300

    edge detection on test video

---
+++ test_video_edges.py
@@ -0,0 +1,20 @@
+import sys, cv2 as cv
+import os
+
+ffmpeg_dll_path = r'E:\python_examples\opencv-3.2.0-vc14\opencv\build\bin'
+os.environ["PATH"] += os.pathsep +  ffmpeg_dll_path
+
+cap = cv.VideoCapture('test_data/t_rex_test_video.avi')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    edges = cv.Canny(gray, 0, 50)
+
+    cv.imshow('frame',edges)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
